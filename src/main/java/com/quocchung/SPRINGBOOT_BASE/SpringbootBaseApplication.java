8bf5d084957a6d8f44da1161e706ba953ccb458f
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-04-24 12:36:52 +0700

    first commit

---
+++ src/main/java/com/quocchung/SPRINGBOOT_BASE/SpringbootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.quocchung.SPRINGBOOT_BASE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBaseApplication.class, args);
+	}
+
+}
